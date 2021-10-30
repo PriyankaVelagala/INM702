2bb79ece4ae9b7b164d86915b01c585076bc54ec
PriyankaVelagala <priyanka@velagala.org>
2021-10-30 11:59:08 +0100

    Add files via upload
    updated greedy to handle corner case when agent backs into a corner  and only use bottom/right paths

--- Task1/helper_functions.py
+++ Task1/helper_functions.py
@@ -41,119 +41,26 @@ def getLegalNodes(currentX, currentY, maxX, maxY):
     #check bottom?
     if currentX + 1 < maxX: nodes.append([currentY, currentX+1])
     #check  up?
-    if currentX - 1 >= 0: nodes.append([currentY, currentX-1])
+    #if currentX - 1 >= 0: nodes.append([currentY, currentX-1])
     #check right
     if currentY + 1 < maxY: nodes.append([currentY+1, currentX])
     #check left 
-    if currentY - 1 >= 0: nodes.append([currentY-1, currentX])
+    #if currentY - 1 >= 0: nodes.append([currentY-1, currentX])
     
     return(nodes)           
         
-    
-"""    
-def greedyPath(mode, costgrid, board):
-    maxX, maxY = costgrid.shape
 
-    pathFound = False
-    
-    if mode == 1:
-        path = [[0,0]] 
-        totalcost = 0 
-        currentX = 0
-        currentY = 0
-        
-        while pathFound == False:
-            toVisit = []
-            allLegalNodes = getLegalNodes(currentX, currentY, maxX, maxY)
-            
-            for node in allLegalNodes:
-                y,x = node 
-                inpath = False
-                for point in path:
-                    if point[0] == x and point[1] == y:
-                        inpath = True
-                        break
-                if inpath == False: toVisit.append([x,y])
-                #if node not in path: toVisit.append(node)
-
-            print("Nodes to visit: ",  [costgrid[x[0]][x[1]] for x in toVisit])
-           
-            cost = float('inf')
-            for x,y in toVisit:
-                if x == maxX-1 and y == maxY-1: 
-                    cost = costgrid[x][y]
-                    pathFound = True 
-                    nextnode = [x,y]
-                    break
-                if costgrid[x][y] < cost:
-                    cost = costgrid[x][y]
-                    nextnode = [x,y]
-
-            totalcost += cost 
-            path.append(nextnode)
-            currentX = nextnode[0]
-            currentY = nextnode[1]
-            board.drawPath(path)
-            
-    elif mode == 2:
-        path = [[0,0]] 
-        totalcost = 0 
-        currentX = 0
-        currentY = 0
-        currentcost = costgrid[0][0]
-        
-        while pathFound == False:
-            toVisit = []
-            allLegalNodes = getLegalNodes(currentX, currentY, maxX, maxY)
-            
-            for node in allLegalNodes:
-                y,x = node 
-                inpath = False
-                for point in path:
-                    if point[0] == x and point[1] == y:
-                        inpath = True
-                        break
-                if inpath == False: toVisit.append([x,y])
-                #if node not in path: toVisit.append(node)
-
-            print("Nodes to visit: ",  [costgrid[x[0]][x[1]] for x in toVisit])
-           
-            cost = float('inf')
-            for x,y in toVisit:
-                currentcost = costgrid[path[-1][0]][path[-1][1]]
-                
-                if x == maxX-1 and y == maxY-1: 
-                    cost = costgrid[x][y]
-                    pathFound = True 
-                    nextnode = [x,y]
-                    break
-                if abs(costgrid[x][y]-currentcost) < cost:
-                    cost = abs(costgrid[x][y]-currentcost)
-                    nextnode = [x,y]
-
-            totalcost += cost 
-            path.append(nextnode)
-            currentX = nextnode[0]
-            currentY = nextnode[1]
-            
-            #print("path: ", [costgrid[x[0]][x[1]] for x in path])
-            #print("total cost: ", totalcost)
-            
-            board.drawPath(path)
-
-        return path
-            
- """           
         
 def greedyPath(mode, costgrid, board):
     maxX, maxY = costgrid.shape
 
     pathFound = False
     path = [[0,0]] 
-    totalcost = 0 
+    #totalcost = 0 
     currentX = 0
     currentY = 0
     currentcost = costgrid[0][0]
+    illegalNodes = []
 
         
     while pathFound == False:
@@ -163,15 +70,24 @@ def greedyPath(mode, costgrid, board):
         for node in allLegalNodes:
             y,x = node 
             inpath = False
-            for point in path:
+            for point in (path + illegalNodes):
                 if point[0] == x and point[1] == y:
                     inpath = True
                     break
             if inpath == False: toVisit.append([x,y])
             #if node not in path: toVisit.append(node)
-
+        print("Current Node: ", costgrid[currentX][currentY])
         print("Nodes to visit: ",  [costgrid[x[0]][x[1]] for x in toVisit])
-       
+        
+        
+        #if agent is backed into a corner 
+        if len(toVisit) == 0:
+            illegalNodes.append(path.pop())
+            currentX = path[-1][0]
+            currentY = path[-1][1]
+            continue 
+        
+                
         cost = float('inf')
         for x,y in toVisit:
             currentcost = costgrid[path[-1][0]][path[-1][1]]
@@ -179,6 +95,11 @@ def greedyPath(mode, costgrid, board):
                 cost = costgrid[x][y]
                 pathFound = True 
                 nextnode = [x,y]
+                print("Nodes to visit: ",  [costgrid[x[0]][x[1]] for x in path])
+                totalcost = 0 
+                for node in path:
+                    cost += costgrid[node[0]][node[1]]
+                print("total cost: ", cost)                    
                 break
             if mode == 1: 
                 if costgrid[x][y] < cost:
@@ -188,11 +109,14 @@ def greedyPath(mode, costgrid, board):
                 if abs(costgrid[x][y]-currentcost) < cost:
                     cost = abs(costgrid[x][y]-currentcost)
                     nextnode = [x,y]
-        totalcost += cost 
+        #totalcost += cost 
         path.append(nextnode)
         currentX = nextnode[0]
         currentY = nextnode[1]
         board.drawPath(path)
+        
+        
+        
     return path
     
 
--- Task1/main.py
+++ Task1/main.py
@@ -27,9 +27,9 @@ MAX_COST = 100
 def main():
     print("Hello!")
     
-    height = 5 #getGridSize("height")
-    width = 5 #getGridSize("width")
-    mode = 2 #1 #getGameMode()
+    height = 10 #getGridSize("height")
+    width = 10 #getGridSize("width")
+    mode = 1 #1 #getGameMode()
     
     print("\nYou choose game mode {}, your grid size is {} x {}".format(mode, height, width))
     costGrid = np.random.randint(0, MAX_COST, (height, width))
@@ -39,8 +39,6 @@ def main():
     print(costGrid.shape)
     path = getShortestPath("greedy", mode, costGrid, myBoard)
     myBoard.drawPath(path)
-
- 
      
    
 if __name__ == "__main__":
